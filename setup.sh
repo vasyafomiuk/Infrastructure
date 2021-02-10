3f029296f70f6f4cc939c1a476085491e311b3d6
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-02-10 11:16:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,9 +14,22 @@ if ! aws cloudformation describe-stacks --stack-name $stackName > /dev/null 2>&1
     --region=us-west-2
 else
  echo -e "\nStack already exist, updating ..."
-  aws cloudformation update-stack \
-    --stack-name $stackName \
-    --template-body file://$stackFile \
-    --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
-    --region=us-west-2
+    output=$(aws cloudformation update-stack --stack-name foo 2>&1)
+    RESULT=$?
+    if [ $RESULT -eq 0 ]; then
+        echo "$output"
+    else
+        if [[ "$output" == *"No updates are to be performed"* ]]; then
+            echo "No cloudformation updates are to be performed."
+            exit 0
+        else
+            aws cloudformation update-stack \
+            --stack-name $stackName \
+            --template-body file://$stackFile \
+            --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+            --region=us-west-2
+            echo "$output"
+            exit $RESULT
+        fi
+    fi
 fi
